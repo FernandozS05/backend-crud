76d29af5112a9633d87ca7a685813f079ba8d9b7
Fernando Larios Sánchez <fernandols0902@gmail.com>
2025-03-30 19:51:10 -0600

    fix: add package CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,POST,PUT,DELETE,PATCH',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   app.setGlobalPrefix("api/v1");
 
   app.useGlobalPipes(
